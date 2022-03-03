9f2b7b3727084c132edb17fcf54a81ca534780a4
Fabrimagic1 <47692828+Fabrimagic1@users.noreply.github.com>
2022-03-03 16:14:29 +0100

    Add files via upload

--- Nuova cartella (3)/enemy.gd
+++ Nuova cartella (3)/enemy.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 
-var run_speed = 25
+var run_speed = 40
 var velocity = Vector2.ZERO
 var player = null
 
@@ -10,25 +10,8 @@ func _physics_process(delta):
 		velocity = position.direction_to(player.position) * run_speed
 	velocity = move_and_slide(velocity)
 
-func _on_DetectRadius_body_entered(body):
+func _on_Area2D_body_entered(body):
 	player = body
-	print('ciao')  
-func _on_DetectRadius_body_exited(body):
-	player = null
-	
 
-		
-			
-			
-
-
-
-
-
-
-func _on_Area2D_area_entered(area):
-	pass # Replace with function body.
-
-
-func _on_Area2D_area_exited(area):
-	pass # Replace with function body.
+func _on_Area2D_body_exited(area):
+	pass
