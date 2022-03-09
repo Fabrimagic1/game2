663b36033fced73bc118fc2f48cb1945e367eab8
Fabrimagic1 <47692828+Fabrimagic1@users.noreply.github.com>
2022-03-09 19:38:44 +0100

    Add files via upload

--- Nuova cartella (3)/Node.gd
+++ Nuova cartella (3)/Node.gd
@@ -1,16 +1,10 @@
-extends KinematicBody2D
+extends Node
 
 var velocity = Vector2(0,0)
 var speed = 300
+
 func _physics_process(delta):
 	var collision_info = move_and_collide(velocity.normalized()* delta * speed)
-# var a = 2
-# var b = "t
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-
-
-
--- Nuova cartella (3)/enemy.gd
+++ Nuova cartella (3)/enemy.gd
@@ -3,7 +3,7 @@ extends KinematicBody2D
 var run_speed = 40
 var velocity = Vector2.ZERO
 var player = null
-
+signal killed
 func _physics_process(delta):
 	velocity = Vector2.ZERO
 	if player:
@@ -15,3 +15,9 @@ func _on_Area2D_body_entered(body):
 
 func _on_Area2D_body_exited(area):
 	pass
+	
+
+func _on_Hit_box_area_entered(area):
+	if area.name == 'hurt_bullet':
+		queue_free()
+		emit_signal("killed")
